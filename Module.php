a99843771881a2708cf71e59636692f8c78f79f2
Anders Blenstrup-Pedersen <anders-github@drake-development.org>
2014-10-13 20:23:41 +0200

    Composer added

--- Module.php
+++ Module.php
@@ -10,8 +10,15 @@ class Module {
             $controllerClass = get_class($controller);
             $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
             $config = $e->getApplication()->getServiceManager()->get('config');
-            if (isset($config['module_layouts'][$moduleNamespace])) {
+            
+            if (isset($config['module_layouts'][$moduleNamespace]) && !is_array($config['module_layouts'][$moduleNamespace])) {
                 $controller->layout($config['module_layouts'][$moduleNamespace]);
+            }
+            elseif (isset($config['module_layouts'][$moduleNamespace][$actionName])) {
+                $controller->layout($config['module_layouts'][$moduleNamespace][$actionName]);
+            }
+            elseif(isset($config['module_layouts'][$moduleNamespace]['default'])) {
+                $controller->layout($config['module_layouts'][$moduleNamespace]['default']);
             }
         }, 100);
     }
